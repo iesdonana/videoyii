e2467521d8ddba44abb0465b70df175d8aa5c150
Jose Luis Delgado <joludelgar@gmail.com>
2016-11-29 17:15:42 +0100

    Añadida relación Socios-Peliculas/Corrección de errores

--- models/Socio.php
+++ models/Socio.php
@@ -47,10 +47,10 @@ class Socio extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'numero' => 'Numero',
+            'numero' => 'Número',
             'nombre' => 'Nombre',
-            'direccion' => 'Direccion',
-            'telefono' => 'Telefono',
+            'direccion' => 'Dirección',
+            'telefono' => 'Teléfono',
             'borrado' => 'Borrado',
         ];
     }
@@ -60,6 +60,11 @@ class Socio extends \yii\db\ActiveRecord
      */
     public function getAlquileres()
     {
-        return $this->hasMany(Alquileres::className(), ['socio_id' => 'id'])->inverseOf('socio');
+        return $this->hasMany(Alquiler::className(), ['socio_id' => 'id'])->inverseOf('socio');
+    }
+
+    public function getPeliculas()
+    {
+        return $this->hasMany(Pelicula::className(), ['id' => 'pelicula_id'])->via('alquileres');
     }
 }
