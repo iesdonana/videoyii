3dc17d29793cb7fddead127aac32bb2767e8844a
Juan Carlos Garcia <gjcarlos8@gmail.com>
2016-11-30 20:54:22 +0100

    añadido un upload en el index

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,6 +39,7 @@ AppAsset::register($this);
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Upload', 'url' => ['/site/upload']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
