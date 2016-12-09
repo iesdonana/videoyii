30ef4ab766b83b728430b8de09aed7d6ab0f61c1
Christian Hidalgo <christianhf.chf@gmail.com>
2016-12-09 18:15:33 +0100

    Añade boton para crear alquiler

--- views/alquileres/index.php
+++ views/alquileres/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]);?>
 
     <p>
-        <?= Html::a('Create Alquiler', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Crear Alquiler', ['alquilar'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
