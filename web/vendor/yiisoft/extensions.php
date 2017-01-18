ea340bec53cdf12495cda68e3967f359de9f8ca1
Christian Hidalgo <christianhf.chf@gmail.com>
2017-01-18 21:13:53 +0100

    Añade modificación de la imagen de los usuarios

---
+++ web/vendor/yiisoft/extensions.php
@@ -0,0 +1,15 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-imagine' => 
+  array (
+    'name' => 'yiisoft/yii2-imagine',
+    'version' => '2.1.0.0',
+    'alias' => 
+    array (
+      '@yii/imagine' => $vendorDir . '/yiisoft/yii2-imagine',
+    ),
+  ),
+);
