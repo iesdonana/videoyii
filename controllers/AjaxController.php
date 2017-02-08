7b30e8eb00a2bf6b8d4dda7306e94efa7fff2962
Ricardo Pérez <ricpelo@gmail.com>
2017-02-08 21:16:17 +0100

    Ajax con dos listas desplegables

--- controllers/AjaxController.php
+++ controllers/AjaxController.php
@@ -2,8 +2,25 @@
 
 namespace app\controllers;
 
+use yii\filters\VerbFilter;
+
 class AjaxController extends \yii\web\Controller
 {
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'ajax' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
@@ -11,6 +28,12 @@ class AjaxController extends \yii\web\Controller
 
     public function actionAjax()
     {
-        return json_encode('hola');
+        $info = [
+            '1' => [1 => 'Jerez', 'Sanlúcar', 'El Puerto'],
+            '2' => [1 => 'Camas', 'Lebrija', 'Las Cabezas'],
+            '3' => [1 => 'Lepe', 'Almonte', 'Cortegana'],
+        ];
+        $id = \Yii::$app->request->post('id');
+        return json_encode($info[$id]);
     }
 }
--- views/ajax/index.php
+++ views/ajax/index.php
@@ -4,14 +4,23 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 
-$url = Url::to(['ajax/ajax'], true);
+$url = Url::to(['ajax/ajax']);
 $js = <<<EOT
-    $('#boton').click(function() {
+    $('#provincia').change(function() {
         $.ajax({
             method: 'POST',
             url: '$url',
+            data: {
+                id: $('#provincia option:selected').val()
+            },
             success: function (data, status, event) {
-                $('#contenido').append(data + '<br/>');
+                var d = JSON.parse(data);
+                while ($('#localidades').children().length > 1) {
+                    $('#localidades').children().last().remove();
+                }
+                for (l in d) {
+                    $('#localidades').append('<option value="' + l + '">' + d[l] + '</option>');
+                }
             }
         });
     });
@@ -20,6 +29,11 @@ $this->registerJs($js);
 ?>
 <h1>ajax/index</h1>
 
-<?= Html::button('Púlsame', ['id' => 'boton']) ?>
+<?= Html::dropDownList('provincia', null, [
+        '1' => 'Cádiz',
+        '2' => 'Sevilla',
+        '3' => 'Huelva',
+    ], ['id' => 'provincia', 'prompt' => 'Provincia...']) ?>
 
-<div id="contenido"></div>
+<?= Html::dropDownList('localidades', null, [], [
+    'id' => 'localidades', 'prompt' => 'Localidades...']) ?>
